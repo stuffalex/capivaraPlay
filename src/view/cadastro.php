b2887082081df32f5a652e81fb3970ab07226402
Alexya Moraes <45324381+stuffalex@users.noreply.github.com>
2019-11-20 08:46:08 -0400

    cadastro v1 kamy

---
+++ src/view/cadastro.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="estilos/estiloCadastro.css">
+    <meta charset="utf-8">
+	<title>Cadastro CapivaraPlay</title>
+</head>
+<body> 
+	<div id="altura">
+		<h2>CAPIVARA PLAY</h2>
+	</div>
+	<h1>CADASTRO</h1>
+
+	<form>
+		<input class="campo" type="text" placeholder="Nome Completo">
+		<input class="campo" type="text" placeholder="Email">
+		<input class="campo" type="password" placeholder="Senha">
+		<input class="campo" type="text" placeholder="Celular">
+		<a href="home.php"><button class="botaoLogin" id="botaoLogin">login</button></a>
+		<h4>Já Possui uma conta?<span>Entrar.</span></h4>
+	</form>
+</body>
+</html>
