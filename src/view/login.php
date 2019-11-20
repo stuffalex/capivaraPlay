c20eb9112a3b10470b0ab6a887df401babf5189e
Alexya Moraes <45324381+stuffalex@users.noreply.github.com>
2019-11-20 08:44:39 -0400

    v 1.1 login kamylla

--- src/view/login.php
+++ src/view/login.php
@@ -3,20 +3,21 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="./view/estilos/estilo.css">
+    <link rel="stylesheet" type="text/css" href="estilos/estiloLogin.css">
     <meta charset="utf-8">
 </head>
     <body>
         <h1>CAPIVARA PLAY</h1>
         <div class="icone">
-            <img src="./view/img/capybara-head.png">
+            <img src="./view/images/capybara-head.png">
         </div>
-        <form action="autenticaLogin.php" method="POST" class="">
-            <input class="campo" type="email" id="campo" name="email" placeholder="e-mail">
-            <input class="campo" type="password" id="campo" name="senha" placeholder="senha">
-            <button class="botaoLogin" id="botaoLogin">login</button>
-        </form>
-    
-       
+
+        <input class="campo" type="text" placeholder="e-mail">
+        <a href="./view/cadastro.html"><h4>Nao possui uma conta?</h4></a>
+
+        <input class="campo" type="password" placeholder="senha">
+        <h4>Esqueceu sua senha?</h4>
+
+        <a href="home.html"><button class="botaoLogin" id="botaoLogin">login</button></a>
     </body>
 </html>    
